90bb281d57ef5d09998b66012a8766bff7284acc
Marco A Souza <marcoso@synnex.com>
2021-03-10 15:04:39 -0300

    Serve para Deploy

---
+++ dist/precache-manifest.9a4ce070ff7c9ff50a66b1ed6bb16ece.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b727da827e4f1c5e7826",
+    "url": "/css/app.f621a488.css"
+  },
+  {
+    "revision": "a019e156cb92b2a4e9ce",
+    "url": "/css/chunk-vendors.525ce022.css"
+  },
+  {
+    "revision": "ed17df7ac24015f9741f1153ebb8e1f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b727da827e4f1c5e7826",
+    "url": "/js/app.01e4bf40.js"
+  },
+  {
+    "revision": "a019e156cb92b2a4e9ce",
+    "url": "/js/chunk-vendors.73fb9772.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
